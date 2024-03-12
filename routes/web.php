1dc4f8ff107b004095a3c113396c4a17d7f94c6d
DESKTOP-84I40L3\TruongNDA <Truongnd@absoltech.vn>
2024-03-12 10:00:33 -0700

    authen login logout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/login', [AdminLoginController::class,'index'])->name('admin.login');
+
+
+Route::group(['prefix' => 'admin'], function () {
+
+    Route::group(['middleware' => 'admin.guest'], function () {
+
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+
+    });
+    Route::group(['middleware' => 'admin.auth'], function () { 
+
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+    });
+});
